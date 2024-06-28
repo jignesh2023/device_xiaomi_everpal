4a3af44b2cab9a955a766e38e02207af798e9fcd
Buildbot_Jignesh <jigneshshukla212@gmail.com>
2024-06-28 16:27:10 +0530

    build spark

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,37 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    GoogleCamera \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    Videos \
+    Video \
+    SafetyHubPrebuilt \
+    ScribePrebuilt \
+    Tycho \
+    Papers \
+    NgaResources \
+    Showcase \
+    Snap \
+    MusicFX \
+    SprintDM \
+    SprintHM \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTube \
+    YouTubeMusicPrebuilt \
+    WellbeingPrebuilt \
+    DevicePolicyPrebuilt \
+    PrebuiltDeskClockGoogle
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
